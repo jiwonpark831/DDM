2e813d58b212a4f886277c754694a6009f35692d
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-10-31 01:14:35 +0900

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 // import 'home.dart';
+import 'firebase_options.dart';
 import 'movingStartPage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
